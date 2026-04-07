782e744c4b5f848377fdc7d639bfb32e6b9aad6b
TOMAS VINICIO LOPEZ <49189962@ort>
2026-04-07 15:00:16 -0300

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
 import Cita from './Cita.jsx'
 import ListaCitas from './ListaCitas.jsx'
 import './App.css'
 
 function App() {
+  const [citas, setCitas] = useState([])
+  const agregarCita = (nuevaCita) => {
+    setCitas([...citas, nuevaCita])
+  }
   return (
-    <main>
-      <h1>ADMINISTRADOR DE PACIENTES</h1>
-      <div className="container">
-        <div className="row">
-          <div className="one-half column">
-            <h2>Crear mi Cita</h2>
-            <Cita />
-          </div>
+    <>
+      <main>
+        <h1>ADMINISTRADOR DE PACIENTES</h1>
+        <div className="container">
+          <div className="row">
+            <div className="one-half column">
+              <h2>Crear mi Cita</h2>
+              <Cita agregarCita={agregarCita} />
+            </div>
 
-          <div className="one-half column">
-            <h2>Administra tus citas</h2>
-            <ListaCitas />
+            <div className="one-half column">
+              <h2>Administra tus citas</h2>
+              <ListaCitas citas={citas} />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
 
--- src/ListaCitas.jsx
+++ src/ListaCitas.jsx
@@ -5,17 +5,19 @@ import heroImg from './assets/hero.png'
 import AdministrarCita from './AdministrarCita.jsx'
 import './App.css'
 
-function ListaCitas() {
+function ListaCitas({ citas }) {// "{}"porque recibe varios
     return (
-        <>
-            <div className="lista-citas">
-                <AdministrarCita Mascota="Walter" Dueño="Pascal" Fecha="2026-07-06" Hora="06:07" Sintomas="Padece de six seven" />
+        <div>
+            {citas.map((cita, index) => (
+                <div key={index} className="lista-citas">
+                    <AdministrarCita Mascota={cita.mascota} Dueño={cita.dueño} Fecha={cita.fecha} Hora={cita.hora} Sintomas={cita.sintomas} />
+                </div>
+            ))}
+            <AdministrarCita Mascota="Walter" Dueño="Pascal" Fecha="2026-07-06" Hora="06:07" Sintomas="Padece de six seven" />
+            <AdministrarCita Mascota="Luna" Dueño="Camila" Fecha="2026-07-08" Hora="14:30" Sintomas="No quiere comer" />
+            <AdministrarCita Mascota="Rocky" Dueño="Matias" Fecha="2026-07-09" Hora="09:45" Sintomas="Le duele la pata al caminar" />
 
-                <AdministrarCita Mascota="Luna" Dueño="Camila" Fecha="2026-07-08" Hora="14:30" Sintomas="No quiere comer" />
-
-                <AdministrarCita Mascota="Rocky" Dueño="Matias" Fecha="2026-07-09" Hora="09:45" Sintomas="Le duele la pata al caminar" />
-            </div>
-        </>
+        </div>
     )
 }
 
